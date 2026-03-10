f4f698532917986e6a5149eed608a7e5cb966f41
restapi404 <rithuprabhu23@gmail.com>
2026-03-10 21:18:41 +0530

    Add use case 12

---
+++ src/usecase12.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
+
+interface PalindromeStrategy {
+    boolean checkPalindrome(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String input) {
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class usecase12 {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        PalindromeStrategy strategy = new StackStrategy();
+
+        boolean result = strategy.checkPalindrome(input);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + result);
+    }
+}
