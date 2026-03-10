3adec702ee5346a9f9e5a654310fe854a580f059
restapi404 <rithuprabhu23@gmail.com>
2026-03-10 19:35:27 +0530

    Add use case 7

---
+++ src/usecase7.java
@@ -0,0 +1,28 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class usecase7 {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
