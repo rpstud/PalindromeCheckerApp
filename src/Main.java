e7739735258aa61c8c1877c71183fe75807a7f87
Rithu <rp0772@srmist.edu.in>
2026-03-11 10:30:57 +0530

    uc9: Reset to single file Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//public class Main {
-//    public static void main(String[] args) {
-//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//        // to see how IntelliJ IDEA suggests fixing it.
-//        System.out.printf("Hello and welcome!");
-//
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
-//    }
-//}
+public class Main {
+
+    static boolean checkPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return checkPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean isPalindrome = checkPalindrome(input, 0, input.length() - 1);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
